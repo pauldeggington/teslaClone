9f0a495b035c34061a27ad1977c78507d0dfb6d6
Paul <pdeggington@gmail.com>
2019-11-26 22:29:42 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Go to top button
+
 var btn = $('#goToButton');
 
 $(window).scroll(function() {
@@ -13,3 +15,19 @@ btn.on('click', function(e) {
   $('html, body').animate({scrollTop:0}, '300');
 });
 
+// Menuing system
+
+window.nav = {
+  status: false,
+  menu: function(){
+    if (nav.status){
+      // here put class when menu is open - eg:
+      document.getElementById("menu-box").className = "menu-box menuopen";
+      nav.status = false;
+    } else {
+      // here put class when menu is closed - eg:
+      document.getElementById("menu-box").className = "menu-box";
+      nav.status = true;
+    } 
+  }
+}
