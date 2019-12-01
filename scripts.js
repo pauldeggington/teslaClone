5d4f9796bef613a60b454bfd91b65bb43d7c24ff
Paul <pdeggington@gmail.com>
2019-11-30 18:31:02 -0800

    Update Scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Right Side Nav Menu Opening
+// Right Side Menu Open Script
 nav = {
   status: true,
   menu: function(){
@@ -14,3 +14,13 @@ nav = {
     } 
   }
 }
+
+// Hamburger Menu Animations Script
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function() {
+// Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+// Do something else, like open/close menu
+});
